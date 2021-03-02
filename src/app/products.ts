4fa0a70eb22ee396ccfecb95965816b46875de96
nurtai00 <nnn.nurtai00@gmail.com>
2021-03-02 22:59:30 +0600

    11 files modified, 3 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,96 +1,133 @@
 export const products = [
   {
     id: 1,
-    name: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg",
+    name:
+      "AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg",
     price: 199.99,
-    description: 'The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!',
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
     rating: 4.7,
-    source:"https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5"
-    },
+    source:
+      "https://www.amazon.com/Acer-SB220Q-Ultra-Thin-Frame-Monitor/dp/B07CVL2D2S/ref=lp_16225007011_1_5"
+  },
   {
     id: 2,
-    name: 'BenQ MOBIUZ EX2510 24.5 Inch 144Hz IPS Gaming Monitor | HDRi | 1080P 1ms | FreeSync Premium | Speakers Dark Grey',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/61O5ydbBsDL._AC_SL1001_.jpg",
+    name:
+      "BenQ MOBIUZ EX2510 24.5 Inch 144Hz IPS Gaming Monitor | HDRi | 1080P 1ms | FreeSync Premium | Speakers Dark Grey",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/61O5ydbBsDL._AC_SL1001_.jpg",
     price: 93.9,
-    description: 'The BenQ EX2510 is a 24.5″ 1080p 144Hz gaming monitor based on a fast IPS panel with FreeSync and MBR support as well as plenty of extra features including an ergonomic stand, built-in speakers, and a light sensor. It offers good value for the price in comparison to the other models that use the same panel, but there are a lot of great alternatives worth considering at this price range.',
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
     rating: 4.6,
-    source:"https://www.amazon.com/BenQ-EX2510-Monitor-FreeSync-Speakers/dp/B08FF6DJNP"
+    source:
+      "https://www.amazon.com/BenQ-EX2510-Monitor-FreeSync-Speakers/dp/B08FF6DJNP"
   },
   {
     id: 3,
-    name: 'Oecrayy 24" Curved Gaming Monitor 144Hz 1080p 2ms VA Display Monitor, Full HD Eye Care Curved Screen Computer Monitor with HDMI DP Port, Black',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/71JHVXupN3L._AC_SL1500_.jpg",
+    name:
+      'Oecrayy 24" Curved Gaming Monitor 144Hz 1080p 2ms VA Display Monitor, Full HD Eye Care Curved Screen Computer Monitor with HDMI DP Port, Black',
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/71JHVXupN3L._AC_SL1500_.jpg",
     price: 199.9,
-    description: 'Display Performance --- 24” curved panel monitor, 1920x1080 resolution VA Display with 144hz refresh rate will improve clarity on your picture during fast action scenes. Allowing you to enjoy a smooth gaming experience and amazing visuals. 178° Wide Viewing Angles --- The wide viewing angle allows you to completely immerse yourself in all the action and wonderful scenery that provided by the high resolution monitor. Experience a fuller view with more space to take in all the action. Multiple Ports --- HDMI, DP, USB, DC interfaces and 3.5mm Audio In/Out allow you to connect devices you need.',
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
     rating: 3.8,
-    source:"https://www.amazon.com/dp/B08GC76TZL/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B08GC76TZL&pd_rd_w=nrbSe&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=2eiyy&pf_rd_r=MDC1NGPT0RW0V4CDX45Z&pd_rd_r=2865f2c1-3588-47d6-8cdd-25e9b492978c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyTjk0M0c2SzA1N0dWJmVuY3J5cHRlZElkPUEwNTc0Mjk2Mzg0Uk83OEZXSE5CVSZlbmNyeXB0ZWRBZElkPUEwNDcxMjQzM001OU45NTIwVVFPOSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    source:
+      "https://www.amazon.com/dp/B08GC76TZL/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B08GC76TZL&pd_rd_w=nrbSe&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=2eiyy&pf_rd_r=MDC1NGPT0RW0V4CDX45Z&pd_rd_r=2865f2c1-3588-47d6-8cdd-25e9b492978c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyTjk0M0c2SzA1N0dWJmVuY3J5cHRlZElkPUEwNTc0Mjk2Mzg0Uk83OEZXSE5CVSZlbmNyeXB0ZWRBZElkPUEwNDcxMjQzM001OU45NTIwVVFPOSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
   {
     id: 4,
-    name: 'SAMSUNG 27-inch T35F LED Monitor with Border-Less Design, IPS Panel, 75hz, FreeSync, and Eye Saver Mode (LF27T350FHNXZA), Dark Blue Gray',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/61B8Lq5NXmL._AC_SL1000_.jpg",
+    name:
+      "SAMSUNG 27-inch T35F LED Monitor with Border-Less Design, IPS Panel, 75hz, FreeSync, and Eye Saver Mode (LF27T350FHNXZA), Dark Blue Gray",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/61B8Lq5NXmL._AC_SL1000_.jpg",
     price: 129.9,
-    description: 'ALL-EXPANSIVE VIEW: The 3-sided borderless display brings a clean and modern aesthetic to any working environment. In a multi-monitor setup, the displays line up seamlessly for a virtually gapless view without distractions. IPS PANEL: Sit anywhere and have a full technicolor experience. The IPS panel preserves color vividness and clarity across every inch of the screen. Even on a display this wide, tones and shades look completely accurate from virtually any angle, with no color washing',
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
     rating: 4.5,
-    source:"https://www.amazon.com/dp/B08FF3JQ28/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B08FF3JQ28&pd_rd_w=nrbSe&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=2eiyy&pf_rd_r=MDC1NGPT0RW0V4CDX45Z&pd_rd_r=2865f2c1-3588-47d6-8cdd-25e9b492978c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQllXMkhRVUMyNEhPJmVuY3J5cHRlZElkPUEwMzc3Mzk4MVBBWlJQTUM5Vkg2VyZlbmNyeXB0ZWRBZElkPUEwNTAxNjIyMUZEQjEwUktaUDM3VyZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    source:
+      "https://www.amazon.com/dp/B08FF3JQ28/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B08FF3JQ28&pd_rd_w=nrbSe&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=2eiyy&pf_rd_r=MDC1NGPT0RW0V4CDX45Z&pd_rd_r=2865f2c1-3588-47d6-8cdd-25e9b492978c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQllXMkhRVUMyNEhPJmVuY3J5cHRlZElkPUEwMzc3Mzk4MVBBWlJQTUM5Vkg2VyZlbmNyeXB0ZWRBZElkPUEwNTAxNjIyMUZEQjEwUktaUDM3VyZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
-   {
+  {
     id: 5,
-    name: 'Sceptre 34-inch Curved UltraWide 21: 9 Creative LED Monitor 2560x1080 Frameless HDMI DisplayPort Up to 100Hz, Machine Black 2020 (C345W-2560UN)',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/71VyzCYM6fL._AC_SL1500_.jpg",
+    name: "Sceptre 34-inch Curved UltraWide 21",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/71VyzCYM6fL._AC_SL1500_.jpg",
     price: 349.8,
-    description: 'Ultra wide ultra slim curved 21 9 immerses the attention of a larger audience with a 34 inch screen that accurately resembles the contours of the human eye. 1500R curvature the 1500R degree curved design immerses you completely into the gaming landscape allowing you to spot all threats and opportunities before all of your competitors',
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
     rating: 4.6,
-    source:"https://www.amazon.com/dp/B0812DHWCJ/ref=syn_sd_onsite_desktop_36?psc=1&uh_it=afd40bf0dbc3a50179a2bc31bec69ea7_CT&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExRFdYQzhLMDFBTVY5JmVuY3J5cHRlZElkPUEwMDk5NTczSDhBUzBRTzJZWklLJmVuY3J5cHRlZEFkSWQ9QTA0MDA5MzdUNElSWFBHTFFWQ0smd2lkZ2V0TmFtZT1zZF9vbnNpdGVfZGVza3RvcCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    source:
+      "https://www.amazon.com/dp/B0812DHWCJ/ref=syn_sd_onsite_desktop_36?psc=1&uh_it=afd40bf0dbc3a50179a2bc31bec69ea7_CT&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExRFdYQzhLMDFBTVY5JmVuY3J5cHRlZElkPUEwMDk5NTczSDhBUzBRTzJZWklLJmVuY3J5cHRlZEFkSWQ9QTA0MDA5MzdUNElSWFBHTFFWQ0smd2lkZ2V0TmFtZT1zZF9vbnNpdGVfZGVza3RvcCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
   {
     id: 6,
-    name:'LG 32UD60-B 4K UHD Monitor with AMD FreeSync, Black',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/91D2KTmiATL._AC_SL1500_.jpg",
-    price:347.5,
-    description:'32 Inches Class UHD 4K (3840 x 2160) Display (31.5 Inches screen size). DCI P3 95 percent Color Gamut. AMD FreeSync Technology. Height Adjustable Stand. On Screen Control with Screen Split. No USB Type C connectivity',
-    rating:4.5,
-    source:"https://www.amazon.com/dp/B079H5Z4S8?aaxitk=fFUvDzJDwQwWtml8HsiHdw&pd_rd_plhdr=t&ref=dacx_dp_6602491350001_3812150740201"
+    name: "LG 32UD60-B 4K UHD Monitor with AMD FreeSync, Black",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/91D2KTmiATL._AC_SL1500_.jpg",
+    price: 347.5,
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
+    rating: 4.5,
+    source:
+      "https://www.amazon.com/dp/B079H5Z4S8?aaxitk=fFUvDzJDwQwWtml8HsiHdw&pd_rd_plhdr=t&ref=dacx_dp_6602491350001_3812150740201"
   },
-    {
+  {
     id: 7,
-    name:'Philips 278E1A 27" frameless monitor, 4K UHD IPS, 109% sRGB, Speakers, VESA, 4Yr Advance Replacement Warranty',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/71-72O5Z-xL._AC_SL1181_.jpg",
-    price:249.9,
-    description:'Philips E-Line 27" curved frameless LED monitor with 4K UHD 3840x2160 resolution. 109% sRGB color gamut coverage and Philips SmartContrast with Mega Infinity Dynamic Contrast Ratio for deep rich blacks and bright whites. Built-in speakers and wall mountable (VESA compatible) Cable slot in stand for easy cable management and routing for a cleaner look',
-    rating:4.6,
-    source:"https://www.amazon.com/dp/B07ZSGR5TM/ref=sspa_dk_detail_3?psc=1&pd_rd_i=B07ZSGR5TM&pd_rd_w=0nLIo&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=XrmI0&pf_rd_r=JYBH3MFPQN27JSB8YPRK&pd_rd_r=45d58415-bc75-43c5-92a5-c0ffdc2f4d0c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVBTUVNCTVlFSlBWJmVuY3J5cHRlZElkPUEwNTg3NDg5MzUzTlZOVllUMzFXUiZlbmNyeXB0ZWRBZElkPUEwOTI4OTQyMk5FNTdPOVFEN1cwMSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    name:
+      'Philips 278E1A 27" frameless monitor, 4K UHD IPS, 109% sRGB, Speakers, VESA, 4Yr Advance Replacement Warranty',
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/71-72O5Z-xL._AC_SL1181_.jpg",
+    price: 249.9,
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
+    rating: 4.6,
+    source:
+      "https://www.amazon.com/dp/B07ZSGR5TM/ref=sspa_dk_detail_3?psc=1&pd_rd_i=B07ZSGR5TM&pd_rd_w=0nLIo&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=XrmI0&pf_rd_r=JYBH3MFPQN27JSB8YPRK&pd_rd_r=45d58415-bc75-43c5-92a5-c0ffdc2f4d0c&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExMVBTUVNCTVlFSlBWJmVuY3J5cHRlZElkPUEwNTg3NDg5MzUzTlZOVllUMzFXUiZlbmNyeXB0ZWRBZElkPUEwOTI4OTQyMk5FNTdPOVFEN1cwMSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
-    {
+  {
     id: 8,
-    name:'ViewSonic ELITE XG270QC Curved 27 Inch 1ms 1440p 165Hz FreeSync Premium Pro Gaming Monitor with VESA DisplayHDR 400 and Advanced Ergonomics for Esports',
-    imagesource:"https://m.media-amazon.com/images/I/61IsQvCebTL._AC_SL1500_.jpg",
-    price:440,
-    description:'ESPORTS MONITOR: Full HD 1080p resolution, true 1ms (GtG) response time, and ultra-fast 240Hz refresh rate gives you the edge in all your esports matches *240Hz is supported via DisplayPort and HDMI. NVIDIA G-SYNC: Experience uninterrupted gaming with synchronized frame rates, variable overdrive, and ultra-low motion blur. BLUR BUSTERS APPROVED: Tuned by Blur Busters, PureXP blur reduction utilizes backlight strobing, drastically improving motion image clarity in fast-moving objects',
-    rating:4.6,
-    source:"https://www.amazon.com/dp/B07VL5SNG8/ref=syn_sd_onsite_desktop_117?uh_it=f1c0bd39b809e726f72a9e2cb695a76b_CT&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzM0FXQkdXWjdKRUdWJmVuY3J5cHRlZElkPUEwNDczNzI4M0hKUkhYSVNMVUVDQSZlbmNyeXB0ZWRBZElkPUEwMDQxMTE5Mk40SDkzR0pFMkRZQyZ3aWRnZXROYW1lPXNkX29uc2l0ZV9kZXNrdG9wJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1"
+    name:
+      "ViewSonic ELITE XG270QC Curved 27 Inch 1ms 1440p 165Hz FreeSync Premium Pro Gaming Monitor with VESA DisplayHDR 400 and Advanced Ergonomics for Esports",
+    imagesource:
+      "https://m.media-amazon.com/images/I/61IsQvCebTL._AC_SL1500_.jpg",
+    price: 440,
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
+    rating: 4.6,
+    source:
+      "https://www.amazon.com/dp/B07VL5SNG8/ref=syn_sd_onsite_desktop_117?uh_it=f1c0bd39b809e726f72a9e2cb695a76b_CT&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzM0FXQkdXWjdKRUdWJmVuY3J5cHRlZElkPUEwNDczNzI4M0hKUkhYSVNMVUVDQSZlbmNyeXB0ZWRBZElkPUEwMDQxMTE5Mk40SDkzR0pFMkRZQyZ3aWRnZXROYW1lPXNkX29uc2l0ZV9kZXNrdG9wJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ&th=1"
   },
-    {
+  {
     id: 9,
-    name:'ASUS TUF Gaming VG279QM 27” HDR Monitor, 1080P Full HD (1920 x 1080), Fast IPS, 280Hz, G-SYNC Compatible, Extreme Low Motion Blur Sync (ELMB SYNC), 1ms, DisplayHDR 400',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/71vH6F0tVML._AC_SL1080_.jpg",
-    price:389.8,
-    description:'27-inch Full HD (1920 x 1080) Fast IPS gaming monitor with ultrafast 280Hz refresh rate (overclocked) designed for professional gamers and immersive gameplay. ASUS Extreme Low Motion Blur Sync (ELMB SYNC) technology enables ELMB together with G-SYNC Compatible, eliminating ghosting and tearing for sharp gaming visuals with high frame rates',
-    rating:4.8,
-    source:"https://www.amazon.com/dp/B0845QFDVM/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B0845QFDVM&pd_rd_w=uCA4B&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=0t4GD&pf_rd_r=R6B1EWV5CPZZ0QZAWQ58&pd_rd_r=a19421f7-7fcd-4e17-89c6-95de4588e969&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMEVOQk9XMUwwNlJYJmVuY3J5cHRlZElkPUEwNzkxNDQ0MlM1TjNVTjlTSlRGNyZlbmNyeXB0ZWRBZElkPUEwOTk4NTg3MjhVMjdHQks5QVYzVSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+    name:
+      "ASUS TUF Gaming VG279QM 27” HDR Monitor, 1080P Full HD (1920 x 1080), Fast IPS, 280Hz, G-SYNC Compatible, Extreme Low Motion Blur Sync (ELMB SYNC), 1ms, DisplayHDR 400",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/71vH6F0tVML._AC_SL1080_.jpg",
+    price: 389.8,
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
+    rating: 4.8,
+    source:
+      "https://www.amazon.com/dp/B0845QFDVM/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B0845QFDVM&pd_rd_w=uCA4B&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=0t4GD&pf_rd_r=R6B1EWV5CPZZ0QZAWQ58&pd_rd_r=a19421f7-7fcd-4e17-89c6-95de4588e969&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyMEVOQk9XMUwwNlJYJmVuY3J5cHRlZElkPUEwNzkxNDQ0MlM1TjNVTjlTSlRGNyZlbmNyeXB0ZWRBZElkPUEwOTk4NTg3MjhVMjdHQks5QVYzVSZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
   },
-    {
+  {
     id: 10,
-    name:'BenQ ZOWIE XL2546K 24.5 inch 240Hz Gaming Monitor | 1080P | DyAc+ | Smaller Base | Flexible height & tilt adjustment | XL Setting to Share | Customizable Quick Menu | S-Switch | Shield',
-    imagesource:"https://images-na.ssl-images-amazon.com/images/I/41NB%2Bnn2YeL._AC_SL1000_.jpg",
-    price:499.9,
-    description:'Lightning-fast 240 Hz high refresh rate performance for a smooth PC gaming experience. 120Hz Compatible for PS5 and Xbox Series X. Dynamic Accuracy plus technology makes vigorous in-game actions such as spraying less blurry. The redesigned base takes up less space, allowing gamer more space in their set-up for in-game movements. The redesigned S-switch remote control allows 4-way scrolling; new Shield for easy assembling with screwless design.',
-    rating:4.9,
-    source:"https://www.amazon.com/BenQ-XL2546K-Flexible-adjustment-Customizable/dp/B08G57L985/ref=sr_1_2?dchild=1&keywords=zowie&qid=1614157451&sr=8-2"
-  },
+    name:
+      "BenQ ZOWIE XL2546K 24.5 inch 240Hz Gaming Monitor | 1080P | DyAc+ | Smaller Base | Flexible height & tilt adjustment | XL Setting to Share | Customizable Quick Menu | S-Switch | Shield",
+    imagesource:
+      "https://images-na.ssl-images-amazon.com/images/I/41NB%2Bnn2YeL._AC_SL1000_.jpg",
+    price: 499.9,
+    description:
+      "The Acer SB220Q offers an amazing image quality and performance for its price thanks to its IPS panel, 1080p resolution, AMD FreeSync, 75Hz refresh rate, and numerous gaming features. It also boasts a modern slim design with ultra-thin bezels!",
+    rating: 4.9,
+    source:
+      "https://www.amazon.com/BenQ-XL2546K-Flexible-adjustment-Customizable/dp/B08G57L985/ref=sr_1_2?dchild=1&keywords=zowie&qid=1614157451&sr=8-2"
+  }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
